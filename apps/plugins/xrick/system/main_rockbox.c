aaebcc10995c9afd67ec44426297057ad269a608
Moshe Piekarski <dev.rockbox@melachim.net>
2020-11-17 13:59:24 -0500

    xrick memory usage
    Change-Id: I45dafe0d6bdc39af390fe8e76f61880083369f1f

--- apps/plugins/xrick/system/main_rockbox.c
+++ apps/plugins/xrick/system/main_rockbox.c
@@ -26,16 +26,73 @@
 #include "xrick/game.h"
 
 #include "plugin.h"
+static const unsigned int sentinel = 0xBA5EFAC7;
+ 
+#define SENTINEL(n) (sentinel ^ (n))
+
+static size_t check_sentinel(void* buf, size_t size)
+ 
+{
+ 
+    const size_t sz = size / sizeof(sentinel);
+ 
+    size_t unused = 0;
+ 
+    size_t i;
+ 
+    unsigned int *b = (int*) buf;
+ 
+    for(i = 0; i < sz; i++)
+ 
+        if (b[i] == SENTINEL(i))
+ 
+        {
+ 
+            unused++;
+ 
+            while(++i < sz && b[i] == SENTINEL(i) && ++unused)
+ 
+                ;;
+ 
+        }
+ 
+        DEBUGF("Unused: %ld\n", unused*sizeof(sentinel));
+        return 1;
+ 
+}
+
+
+static void set_sentinel(void* buf, size_t size)
+ 
+{
+ 
+    size_t i;
+ 
+    unsigned int *b = (int*) buf;
+ 
+    for(i = 0; i < size / sizeof(sentinel); i++)
+ 
+        *b++ = SENTINEL(i);
+ 
+}
 
 /* Plug-in entry point */
 enum plugin_status plugin_start(const void* parameter)
 {
+    size_t size, size2;
+    char *buf = rb->plugin_get_buffer(&size);
+    char *buf2 = rb->plugin_get_audio_buffer(&size2);
+    DEBUGF("Size: %ld\nSize2: %ld\n", size, size2);
+    set_sentinel(buf, size);
+    set_sentinel(buf2, size2);
     char *filename = (char*)parameter;
     bool success = sys_init(1, &filename);
     if (success)
     {
         game_run();
     }
+    check_sentinel(buf, size);
+    check_sentinel(buf2, size2);
     sys_shutdown();
     return (success? PLUGIN_OK : PLUGIN_ERROR);
 }
